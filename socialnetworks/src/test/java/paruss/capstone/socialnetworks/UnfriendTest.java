526a64b1efeb391fb51ab950f9f443bc9609a249
RUSSELLP1 <russellp@theaa.ie>
2016-12-19 11:54:26 +0000

    Base structure set up but need to add full implementation of graph class and write first test.

---
+++ socialnetworks/src/test/java/paruss/capstone/socialnetworks/UnfriendTest.java
@@ -0,0 +1,13 @@
+package paruss.capstone.socialnetworks;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UnfriendTest 
+{
+	@Test
+	public void givenHasNoFriendsReturnsEmptyList(){
+	}
+}
